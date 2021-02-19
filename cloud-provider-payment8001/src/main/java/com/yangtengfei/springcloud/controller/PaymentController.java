c13d29a767aeab5f518044198c01ecf085a2f68d
Administrator <asdasd>
2021-02-19 14:24:30 +0800

    commit

--- cloud-provider-payment8001/src/main/java/com/yangtengfei/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/yangtengfei/springcloud/controller/PaymentController.java
@@ -1,15 +1,19 @@
 package com.yangtengfei.springcloud.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.yangtengfei.springcloud.common.bean.CommonResult;
 import com.yangtengfei.springcloud.entities.Payment;
 import com.yangtengfei.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -17,6 +21,24 @@ public class PaymentController {
 
     @Resource
     private PaymentService paymentService;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/findDiscovery")
+    public CommonResult discoveryClient() {
+
+        List<String> services = discoveryClient.getServices();
+
+        log.info("services:{}", JSON.toJSONString(services));
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for(ServiceInstance serviceInstance:instances){
+            log.info(serviceInstance.getInstanceId()+"\t"+serviceInstance.getHost()+"\t"+serviceInstance.getPort()+"\t"+serviceInstance.getUri());
+        }
+        //log.info("instances:{}", JSON.toJSONString(instances));
+
+        return new CommonResult(200, "success", null);
+    }
 
     @PostMapping("/insert")
     public CommonResult commonResult(@RequestBody Payment payment) {
