7854f0a0ea7a30ec0553e4b614ae99fae1662003
Administrator <asdasd>
2021-02-20 11:32:12 +0800

    commit

--- cloud-consumer-order80/src/main/java/com/yangtengfei/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/yangtengfei/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.yangtengfei.springcloud.controller;
 import com.yangtengfei.springcloud.common.bean.CommonResult;
 import com.yangtengfei.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,5 +29,15 @@ public class OrderController {
     @GetMapping("/getPaymentById")
     public CommonResult getPaymentById(Long id) {
         return restTemplate.getForObject(PAYMENT_URL + "/getPaymentById?id=" + id, CommonResult.class);
+    }
+
+    @GetMapping("/getPaymentById2")
+    public CommonResult ResponseEntity(Long id) {
+        //return restTemplate.getForObject(PAYMENT_URL + "/getPaymentById?id=" + id, CommonResult.class);
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/getPaymentById?id=" + id, CommonResult.class);
+        if(entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        }
+        return new CommonResult(444,"操作失败");
     }
 }
